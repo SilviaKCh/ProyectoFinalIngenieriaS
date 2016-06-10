7cb2fb8f9ae62c7e7354f19992337dfe70d6011c
SilviaKCh <chivis.tt4@gmail.com>
2016-06-09 22:56:13 -0600

    nombre

--- CalculadoraIng_Soft/CalculadoraIng_Soft/Form1.Designer.cs
+++ CalculadoraIng_Soft/CalculadoraIng_Soft/Form1.Designer.cs
@@ -265,7 +265,7 @@
             this.Controls.Add(this.btnborrar);
             this.Controls.Add(this.txtdisplayC);
             this.Name = "Form1";
-            this.Text = "Calculadora";
+            this.Text = "Calcu";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
